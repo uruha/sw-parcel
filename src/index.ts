514ae51afabc29d0ee2060cf69e150941a6d4b6d
uruha <kochmusica@gmail.com>
2018-11-11 14:47:08 +0900

    fix load sw timing

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,17 @@ import message from './message';
 console.log(message);
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/worker.ts')
-        .then(registration => {
-            console.log(
-                'ServiceWorker registration successful with scope: ',
-                registration.scope
-            );
-        })
-        .catch(err => {
-            console.log('ServiceWorker registration falled: ', err);
-        });
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/worker.ts')
+            .then(registration => {
+                console.log(
+                    'ServiceWorker registration successful with scope: ',
+                    registration.scope
+                );
+            })
+            .catch(err => {
+                console.log('ServiceWorker registration falled: ', err);
+            });
+    });
 }
