2edaa51be2bd9ed8ee1054c1f32fa3abc3da41d1
uruha <kochmusica@gmail.com>
2018-11-11 13:57:45 +0900

    add worker

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 import message from './message';
 
 console.log(message);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/worker.ts')
+        .then(registration => {
+            console.log(
+                'ServiceWorker registration successful with scope: ',
+                registration.scope
+            );
+        })
+        .catch(err => {
+            console.log('ServiceWorker registration falled: ', err);
+        });
+}
---
+++ src/worker.ts
@@ -0,0 +1,37 @@
+const ctx: ServiceWorker = self as any;
+const CACHE_NAME = 'sw-cache-v1';
+const urlsToCache = ['src.77de5100.js'];
+
+ctx.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+ctx.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) return response;
+            const fetchRequest = event.request.clone();
+            return fetch(fetchRequest).then(response => {
+                if (
+                    !response ||
+                    response.status !== 200 ||
+                    response.type !== 'basic'
+                ) {
+                    return response;
+                }
+
+                const responseToCache = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            });
+        })
+    );
+});
